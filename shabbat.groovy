129a9926d44b90d54d70753903b6cf0c88fc38c4
Josh Rosenberg <josh@josh-rosenberg.com>
2026-05-25 13:48:18 -0400

    claude: remove 'section' (legacy smartthings structure)

--- shabbat.groovy
+++ shabbat.groovy
@@ -27,13 +27,11 @@ metadata {
 }
 
 preferences {
-    section("URIs") {
-        input name: "daysOfChag", type: "enum", options: ["1","2"], title: "Days of chag", description: "Number of days to observe chag (does not apply to Rosh Hashanah or Yom Kippur)", required: true
-        input name: "hubVarStartTime", type: "string", title: "Hub variable for Shabbat/chag start", description: "Name of an already-created Hub variable of type DateTime. If Shabbat or a holiday starts today, that start time will be assigned to the specified variable.", required: false
-        input name: "hubVarEndTime", type: "string", title: "Hub variable for Shabbat/chag end", description: "Name of an already-created Hub variable of type DateTime. If Shabbat or a holiday ends today, that end time (as 8.5 degrees) will be assigned to the specified variable.", required: false
-        input name: "debugOffset", type: "Number", title: "Debug Offset", description: "Number of minutes in the future (or past if negative) to pretend it is right now. May be weird; for debug purposes only.", required: false
-        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
-    }
+    input name: "daysOfChag", type: "enum", options: ["1","2"], title: "Days of chag", description: "Number of days to observe chag (does not apply to Rosh Hashanah or Yom Kippur)", required: true
+    input name: "hubVarStartTime", type: "string", title: "Hub variable for Shabbat/chag start", description: "Name of an already-created Hub variable of type DateTime. If Shabbat or a holiday starts today, that start time will be assigned to the specified variable.", required: false
+    input name: "hubVarEndTime", type: "string", title: "Hub variable for Shabbat/chag end", description: "Name of an already-created Hub variable of type DateTime. If Shabbat or a holiday ends today, that end time (as 8.5 degrees) will be assigned to the specified variable.", required: false
+    input name: "debugOffset", type: "Number", title: "Debug Offset", description: "Number of minutes in the future (or past if negative) to pretend it is right now. May be weird; for debug purposes only.", required: false
+    input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
 }
 
 // standard callback methods
